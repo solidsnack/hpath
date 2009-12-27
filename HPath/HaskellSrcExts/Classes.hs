c341573f8894360097f082acd91b1ff1584dd7cd
Jason Dusek <jason.dusek@gmail.com>
2009-12-27 02:14:03 -0800

    Renaming this further since we want to have a place to stick things that adapters.

--- HPath/HaskellSrcExts/Classes.hs
+++ HPath/HaskellSrcExts/Classes.hs
@@ -1,5 +1,5 @@
 
-module HPath.HaskellSrcExts where
+module HPath.HaskellSrcExts.Classes where
 
 import Language.Haskell.Exts.Annotated
 import Data.List
@@ -10,6 +10,7 @@ import Data.List
 class (Annotated ast) => SearchModule ast where
   declarations              ::  Module t -> ast t -> [Decl t]
 
+
 instance SearchModule Name where
   declarations mod name      =  case mod of
     Module _ _ _ _ decls    ->  filter' decls
@@ -17,6 +18,7 @@ instance SearchModule Name where
     _                       ->  []
    where
     filter'                  =  filter (`match` name)
+
 
 instance SearchModule QName where
   declarations m qname =
@@ -27,23 +29,30 @@ instance SearchModule QName where
                                   else  []
       _                     ->  []
 
+
 instance SearchModule Op where
   declarations m (VarOp _ name) = declarations m name
   declarations m (ConOp _ name) = declarations m name
+
 
 instance SearchModule QOp where
   declarations m (QVarOp _ qname) = declarations m qname
   declarations m (QConOp _ qname) = declarations m qname
 
 
+
+
 class (Annotated ast) => HasModuleName ast where
   module_name               ::  ast t -> String
+
 
 instance HasModuleName ModuleName where
   module_name (ModuleName _ s) = s
 
+
 instance HasModuleName ModuleHead where
   module_name (ModuleHead _ name _ _) = module_name name 
+
 
 instance HasModuleName Module where
   module_name (Module _ (Just head) _ _ _) = module_name head
@@ -51,17 +60,22 @@ instance HasModuleName Module where
   module_name (XmlHybrid _ (Just head) _ _ _ _ _ _ _) = module_name head
   module_name _              =  ""
 
+
 instance HasModuleName QName where
   module_name (Qual _ name _) = module_name name
   module_name _              =  ""
+
 
 instance HasModuleName QOp where
   module_name (QVarOp _ qname) = module_name qname
   module_name (QConOp _ qname) = module_name qname
 
 
+
+
 class (Annotated ast) => MentionsNames ast where
   match                     ::  ast t -> Name t -> Bool
+
 
 instance MentionsNames Decl where 
   match decl name            =  case decl of
@@ -78,21 +92,25 @@ instance MentionsNames Decl where
     ForExp _ _ _ name' _    ->  match name name'
     _                       ->  False
 
+
 instance MentionsNames DeclHead where
   match head name            =  case head of
     DHead _ name' _         ->  match name name'
     DHInfix _ _ name' _     ->  match name name'
     DHParen _ head'         ->  match head' name
 
+
 instance MentionsNames Op where
   match op name              =  case op of
     VarOp _ name'           ->  match name name'
     ConOp _ name'           ->  match name name'
 
+
 instance MentionsNames Match where
   match m name               =  case m of
     Match _ name' _ _ _     ->  match name name'
     InfixMatch _ _ n _ _ _  ->  match name n
+
 
 instance MentionsNames Name where
   match (Ident _ s) (Ident _ s') = s == s'
