b641395d596db9921ee15781be0305cd3f90bd47
Jason Dusek <jason.dusek@gmail.com>
2009-12-29 22:13:09 -0800

    Much more debuggable; I think I know what's wrong with the parser.

--- HPath/HaskellSrcExts.hs
+++ HPath/HaskellSrcExts.hs
@@ -1,6 +1,9 @@
 
 module HPath.HaskellSrcExts where
 
+import Prelude hiding (readFile)
+import System.IO.UTF8
+import Data.Either
 import Data.List
 
 import Language.Haskell.Extension as Cabal
@@ -31,3 +34,24 @@ extension_conversion        ::  [Cabal.Extension] -> [HaskellSrcExts.Extension]
 extension_conversion         =  impliesExts . fmap (classifyExtension . show)
 
 
+modules
+ :: [FilePath]
+ -> [HaskellSrcExts.Extension]
+ -> IO ( [Module SrcSpanInfo]
+       , ([(SrcLoc, String)], [(FilePath, IOError)]))
+modules paths exts           =  do
+  runs                      <-  sequence (fmap carefully paths)
+  let (exceptions, parses)   =  partitionEithers runs
+      (ok, err)              =  partition parses
+  return (ok, (err, exceptions))
+ where
+  carefully f                =  catch (Right `fmap` read_mod f) paired_exc
+   where
+    paired_exc               =  return . Left . ((,) f)
+  read_mod f                 =  parseModuleWithMode (mode f) `fmap` readFile f
+  mode f                     =  ParseMode f exts False fixities
+  ParseMode _ _ _ fixities   =  defaultParseMode
+  partition                  =  foldr part ([], [])
+   where
+    part (ParseOk t) (ok, err) = (t:ok, err)
+    part (ParseFailed loc s) (ok, err) = (ok, (loc, s):err)
--- Main.hs
+++ Main.hs
@@ -2,42 +2,64 @@
 
 import Prelude hiding (print)
 import System.Directory
+import System.FilePath
+import Data.List
 import Data.Either
 import Data.Maybe
+import Control.Monad
 
 import System.Environment.UTF8
 import System.IO.UTF8
 import System.IO (stderr, stdin, stdout)
+import Language.Haskell.Exts.Annotated.ExactPrint
 
-import HPath.Batch
 import HPath.Path
+import HPath.Hierarchy
+import qualified HPath.HaskellSrcExts as HaskellSrcExts
+import qualified HPath.Cabal as Cabal
+
 
 
 
 usage name                   =  unlines
- [ "USAGE:   " ++ name ++ " <Haskell identifiers>"
+ [ "USAGE:   " ++ name ++ " <Haskell identifier>"
  , ""
- , "  Print the source text corresponding to the Haskell identifiers."
+ , "  Print the source text corresponding to the Haskell identifier, assuming"
+ , "  we are in a project directory where this source can be found."
  , ""
  ]
 
 
-
 main                         =  do
   args                      <-  getArgs
-  name                      <-  getProgName
+  usage'                    <-  usage `fmap` getProgName
   case args of
-    ["-h"]                  ->  out (usage name)
-    ["-?"]                  ->  out (usage name)
-    ["--help"]              ->  out (usage name)
+    ["-h"]                  ->  out usage'
+    ["-?"]                  ->  out usage'
+    ["--help"]              ->  out usage'
     [arg]                   ->  case parse arg of
       Left e                ->  do
         err ("Not a path: " ++ arg ++ "\n" ++ show e)
-        err (usage name)
+        err usage'
       Right path            ->  do
+        err (url path)
         dir                 <-  getCurrentDirectory
-        (_, texts):_        <-  fst `fmap` batch [path] dir
-        mapM_ out texts
+        (exts, roots)       <-  Cabal.info dir
+        let files            =  nub [ r </> p | p <- paths path, r <- roots ]
+            converted        =  nub (HaskellSrcExts.extension_conversion exts)
+        (mods, errs)        <-  HaskellSrcExts.modules files converted
+        let parse_errors     =  fst errs
+            io_exceptions    =  snd errs
+        when ((not . null) parse_errors)
+             (err "Parse errors:" >> mapM_ print parse_errors)
+        when ((not . null) io_exceptions)
+             (err "File I/O errors:" >> mapM_ print io_exceptions)
+        if null mods
+          then  err "No files corresponding to this identifier." >> err usage'
+          else  do
+            let decls        =  HaskellSrcExts.search path (take 1 mods)
+            mapM_ (out . (`exactPrint` [])) decls
+    _                       ->  err usage'
 
 
 err s                        =  hPutStrLn stderr s
